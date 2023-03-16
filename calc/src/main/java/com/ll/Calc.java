280acdada89cfe7e96cf08b09cdd694e307b099b
OH YURA <102283560+yuraoh12@users.noreply.github.com>
2023-03-16 21:28:58 +0900

    재귀 함수 호출 구조파악을 위한 더버그 기능

---
+++ calc/src/main/java/com/ll/Calc.java
@@ -0,0 +1,113 @@
+package com.ll;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Calc {
+    public static boolean debug = false;
+    public static int runCallCount = 0;
+
+    public static int run(String exp) {
+        runCallCount++;
+
+        exp = exp.trim();
+        exp = stripOuterBrackets(exp);
+
+        if (debug) {
+            System.out.printf("exp(%d) : %s\n", runCallCount, exp);
+        }
+
+
+        // 단일항이 입력되면 바로 리턴
+        if (!exp.contains(" ")) return Integer.parseInt(exp);
+
+        boolean needToMulti = exp.contains(" * ");
+        boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
+        boolean needToCompound = needToMulti && needToPlus;
+        boolean needToSplit = exp.contains("(") || exp.contains(")");
+
+        if (needToSplit) {
+            int splitPointIndex = findSplitPointIndex(exp);
+
+            String firstExp = exp.substring(0, splitPointIndex);
+            String secondExp = exp.substring(splitPointIndex + 1);
+
+            char operationCode = exp.charAt(splitPointIndex);
+
+            exp = Calc.run(firstExp) + " " + operationCode + " " + Calc.run(secondExp);
+
+            return Calc.run(exp);
+        } else if (needToCompound) {
+            String[] bits = exp.split(" \\+ ");
+
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Calc::run)
+                    .mapToObj(e -> e + "")
+                    .collect(Collectors.joining(" + "));
+
+            return run(newExp);
+        } else if (needToPlus) {
+            exp = exp.replaceAll("- ", "+ -");
+
+            String[] bits = exp.split(" \\+ ");
+
+            int sum = 0;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        } else if (needToMulti) {
+            String[] bits = exp.split(" \\* ");
+
+            int sum = 1;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+
+        throw new RuntimeException("올바른 계산식이 아닙니다.");
+    }
+
+    private static int findSplitPointIndexBy(String exp, char findChar) {
+        int bracketsCount = 0;
+
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+
+            if (c == '(') {
+                bracketsCount++;
+            } else if (c == ')') {
+                bracketsCount--;
+            } else if (c == findChar) {
+                if (bracketsCount == 0) return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private static int findSplitPointIndex(String exp) {
+        int index = findSplitPointIndexBy(exp, '+');
+
+        if (index >= 0) return index;
+
+        return findSplitPointIndexBy(exp, '*');
+    }
+
+    private static String stripOuterBrackets(String exp) {
+        int outerBracketsCount = 0;
+
+        while (exp.charAt(outerBracketsCount) == '(' && exp.charAt(exp.length() - 1 - outerBracketsCount) == ')') {
+            outerBracketsCount++;
+        }
+
+        if (outerBracketsCount == 0) return exp;
+
+        return exp.substring(outerBracketsCount, exp.length() - outerBracketsCount);
+    }
+}
